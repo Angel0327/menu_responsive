36fdfe9d754848d4373b8d54808138d295206920
Angel0327 <66433690+Angel0327@users.noreply.github.com>
2020-08-25 14:45:26 -0600

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,11 +1,16 @@
-var Botonlist4= document.getElementById("listBoton4");
-var Boton4=document.getElementById('boton4')
+var Botonlis4 = document.getElementById("listBoton4");
+var Boton4 = document.getElementById("boton4");
 
 
 function desplegarBoton4 (){
-	Botonlist4.classList.add('mostrar');
-	
-};
+	Botonlis4.classList.add("mostrar");
+}
+
+
+function ocultarBoton4() {
+    Botonlis4.classList.remove("mostrar");
+}
 
 
 boton4.addEventListener("mouseover",desplegarBoton4,true);
+boton4.addEventListener("mouseout", ocultarBoton4, true);
